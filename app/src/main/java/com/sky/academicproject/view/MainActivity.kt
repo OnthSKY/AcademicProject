1e2c7f96fe07d7e7b0fd982a13215da6b810e997
SKY <onthsky@gmail.com>
2022-05-25 15:45:48 +0300

    Initial Commit

---
+++ app/src/main/java/com/sky/academicproject/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sky.academicproject.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sky.academicproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
