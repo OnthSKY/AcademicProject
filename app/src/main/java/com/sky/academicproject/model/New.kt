0f43b452f9d95a751546dd48adf6a8e3545dba80
SKY <onthsky@gmail.com>
2022-05-25 18:11:11 +0300

    new codes has adde by SKY

--- app/src/main/java/com/sky/academicproject/model/New.kt
+++
@@ -1,4 +0,0 @@
-package com.sky.academicproject.model
-
-class New {
-}
--- app/src/main/java/com/sky/academicproject/view/MainActivity.kt
+++ app/src/main/java/com/sky/academicproject/view/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.sky.academicproject.view
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.sky.academicproject.R
+import com.sky.academicproject.viewmodel.NewViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  viewModel : NewViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(NewViewModel::class.java)
+        viewModel.getData("bitcoin",5)
+       // viewModel.getDataDirect()
+        observeLiveData()
+    }
+
+    private fun observeLiveData()
+    {
+        println("Observe Live Data içerisi")
+        viewModel.response.observe(this, Observer{ response->
+            response?.let {
+                println("Let içerisi")
+                if(it.status == "ok")
+                {
+                    println("Ok içerisi")
+                    textView.text = it.articles!!.size.toString()
+                }
+                else
+                {
+                    textView.text = it.status
+                }
+            }
+        })
+
     }
 }
