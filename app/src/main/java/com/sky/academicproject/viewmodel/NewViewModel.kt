dff904a7ce5d22c3025dab0d75787d191868f7c9
SKY <onthsky@gmail.com>
2022-05-28 15:28:38 +0300

    async, normal and coroutine codes that get data from internet has fixed

--- app/src/main/java/com/sky/academicproject/viewmodel/NewViewModel.kt
+++ app/src/main/java/com/sky/academicproject/viewmodel/NewViewModel.kt
@@ -1,15 +1,18 @@
 package com.sky.academicproject.viewmodel
 
 import android.app.Application
+
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.sky.academicproject.model.Response
 import com.sky.academicproject.sevice.NewAPIService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
 
 class NewViewModel(application: Application) : BaseViewModel(application) {
 
@@ -18,43 +21,34 @@ class NewViewModel(application: Application) : BaseViewModel(application) {
 
     val response = MutableLiveData<Response>()
 
-    fun getData(word: String,pageSize: Int )
+
+    fun getData(word: String, pageSize: Int)
     {
-        getDataFromInternet(word,pageSize)
+        getDataFromInternet(word, pageSize)
     }
-    fun getDataWihtCoroutine(word: String, pageSize: Int)
+
+    fun getDataWithAsync(word: String,pageSize: Int )
+    {
+        getDataAsync(word,pageSize)
+    }
+
+
+    fun getDataWithCoroutine(word: String, pageSize: Int)
     {
         getDataWithCoroutineV1(word, pageSize)
     }
+
     private fun getDataFromInternet( word: String,pageSize: Int)
     {
-
-        disposable.add(
-            api.getData(word,pageSize)
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(object : DisposableSingleObserver<Response>(){
-                    override fun onSuccess(t: Response) {
-                        response.value = t
-                    }
-
-                    override fun onError(e: Throwable) {
-                        e.printStackTrace()
-                    }
-                })
-        )
-    }
-
-    private fun getDataWithCoroutineV1(word:String,pageSize: Int)
-    {
-        launch {
+        val time = measureTimeMillis {
             disposable.add(
-                api.getData(word,pageSize)
+                api.getData(word, pageSize)
                     .subscribeOn(Schedulers.newThread())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeWith(object : DisposableSingleObserver<Response>(){
+                    .subscribeWith(object : DisposableSingleObserver<Response>() {
                         override fun onSuccess(t: Response) {
                             response.value = t
+                           // println("Normal Thread name is ${Thread.currentThread().id}")
                         }
 
                         override fun onError(e: Throwable) {
@@ -63,6 +57,57 @@ class NewViewModel(application: Application) : BaseViewModel(application) {
                     })
             )
         }
+        println("Normal çağırma işleminde geçen süre ${time} ms")
+    }
+
+    private fun getDataWithCoroutineV1(word:String,pageSize: Int)
+    {
+        val time = measureTimeMillis {
+
+            launch {
+                delay(1000L)
+              //  println("Coroutine Launch içerisine girdi")
+                disposable.add(
+                    api.getDataSuspend(word, pageSize)
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(object : DisposableSingleObserver<Response>() {
+                            override fun onSuccess(t: Response) {
+                                response.value = t
+                              //  println("Coroutine Thread name is ${Thread.currentThread().id}")
+                            }
+
+                            override fun onError(e: Throwable) {
+                                e.printStackTrace()
+                            }
+                        })
+                )
+            }
+        }
+        println("Coroutine içerisinde Geçen süre ${time} ms")
+    }
+    private fun getDataAsync(word: String, pageSize: Int)
+    {
+        var time = measureTimeMillis {
+        async {
+                disposable.add(
+                    api.getDataSuspend(word,pageSize)
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(object : DisposableSingleObserver<Response>(){
+                            override fun onSuccess(t: Response) {
+                                response.value = t
+                               // println("Async Thread name is ${Thread.currentThread().state}")
+                            }
+
+                            override fun onError(e: Throwable) {
+                                e.printStackTrace()
+                            }
+                        })
+                )
+            }
+        }
+        println("Async içerisinde geçen süre ${time} ms ")
     }
 
 
